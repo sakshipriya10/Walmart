84a4572fd4119e40dba2606a74645229885d742b
komal-nitp <komalk.ug22.ee@nitp.ac.in>
2025-06-27 17:10:29 +0530

    Login and SignUp Page is Linked with Home Page

--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar.jsx";
 import Hero from "../components/Hero.jsx";
 import FeaturedProducts from "../components/FeaturedProducts";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
      <FeaturedProducts />
-
+    
     </div>
   );
 }
